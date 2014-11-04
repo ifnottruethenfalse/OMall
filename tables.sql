7b818bd16e76adc1db373551499bbd354462c509
YCoro <cristhianmorales18@gmail.com>
2014-11-03 21:48:04 -0600

    Se agregaron tablas para postgresql

---
+++ tables.sql
@@ -0,0 +1,71 @@
+/*Script*/
+
+CREATE TABLE USUARIOS(
+	nit varchar,
+	username varchar,
+	password varchar,
+	nombre varchar,
+	ciudad varchar,
+	pais varchar,
+	correoelectronico varchar,
+	superuser boolean,
+	PRIMARY KEY(username)
+);
+CREATE TABLE Calendario(
+	username varchar,
+	fecha date,
+	nombreevento varchar,
+	PRIMARY KEY(username),
+	FOREIGN KEY(username) REFERENCES USUARIOS
+	ON DELETE CASCADE
+);
+
+CREATE TABLE INBOX(
+	username1 varchar,
+	username2 varchar,
+	Idinbox int,
+	fecha date,
+	description varchar,
+	PRIMARY KEY(Idinbox),
+	FOREIGN KEY(username1) REFERENCES USUARIOS
+	FOREIGN KEY(username2) REFERENCES USUARIOS
+	ON DELETE CASCADE
+);
+CREATE TABLE AMIGO(
+	username1 varchar,
+	username2 varchar,
+	Idamigo int,
+	fecha date,
+	description varchar,
+	PRIMARY KEY(Idamigo),
+	FOREIGN KEY(username1) REFERENCES USUARIOS
+	FOREIGN KEY(username2) REFERENCES USUARIOS
+	ON DELETE CASCADE
+);
+
+CREATE TABLE PRODUCTOS(
+	IDproducto int,
+	description varchar,
+	name varchar,
+	PRIMARY KEY(IDproducto)
+);
+
+CREATE TABLE TIENE(
+	username varchar,
+	IDproducto int,
+	PRIMARY KEY(username,IDproducto),
+	FOREIGN KEY(username) REFERENCES USUARIOS
+	FOREIGN KEY(IDproducto) REFERENCES PRODUCTOS
+);
+
+CREATE TABLE Ordenes(
+	description varchar,
+	username1 varchar,
+	username2 varchar,
+	IDproducto int
+	IDorden int,
+	PRIMARY KEY(IDorden),
+	FOREIGN KEY(username1) REFERENCES USUARIOS
+	FOREIGN KEY(username2) REFERENCES USUARIOS
+	FOREIGN KEY(IDproducto) REFERENCES PRODUCTOS
+);
