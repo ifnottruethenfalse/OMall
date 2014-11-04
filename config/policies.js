3834b6ad9d937fb6262dc4dc44e924c63a488859
YCoro <cristhianmorales18@gmail.com>
2014-11-04 02:56:00 -0600

    Added guest photo and changed static pages

--- config/policies.js
+++ config/policies.js
@@ -26,7 +26,7 @@ module.exports.policies = {
   *                                                                          *
   ***************************************************************************/
 
-  'UserController': {
+  /*'UserController': {
     '*':  'isAuthenticated'
 
   },
